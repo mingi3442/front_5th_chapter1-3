bda28461e5b431d1a6121adff9bffb34a343f893
Lee MinGi <mingi3442@gmail.com>
2025-04-10 18:40:18 +0900

    🔧 Udpate vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 
 export default mergeConfig(
   defineConfig({
-    base: process.env.NODE_ENV === "production" ? "/front_5th_chapter1-2/" : "",
+    base: process.env.NODE_ENV === "production" ? "/front_5th_chapter1-3/" : "",
 
     plugins: [react()],
   }),
